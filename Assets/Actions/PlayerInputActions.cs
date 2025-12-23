5e67d99cbe25160a4199c2366cfd7f3735993620
kemini <moeday1010@gmail.com>
2025-12-23 22:33:42 +0900

    플레이어 대시 이동 구현

--- Assets/Actions/PlayerInputActions.cs
+++ Assets/Actions/PlayerInputActions.cs
@@ -100,6 +100,15 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""acd6b579-7101-43c8-a2ed-9345aeaf0d96"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -157,6 +166,17 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a9bee909-6f88-4fd8-8e37-4d32a903203b"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -166,6 +186,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         // Movement
         m_Movement = asset.FindActionMap("Movement", throwIfNotFound: true);
         m_Movement_Move = m_Movement.FindAction("Move", throwIfNotFound: true);
+        m_Movement_Dash = m_Movement.FindAction("Dash", throwIfNotFound: true);
     }
 
     ~@PlayerInputActions()
@@ -247,6 +268,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Movement;
     private List<IMovementActions> m_MovementActionsCallbackInterfaces = new List<IMovementActions>();
     private readonly InputAction m_Movement_Move;
+    private readonly InputAction m_Movement_Dash;
     /// <summary>
     /// Provides access to input actions defined in input action map "Movement".
     /// </summary>
@@ -262,6 +284,10 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Movement/Move".
         /// </summary>
         public InputAction @Move => m_Wrapper.m_Movement_Move;
+        /// <summary>
+        /// Provides access to the underlying input action "Movement/Dash".
+        /// </summary>
+        public InputAction @Dash => m_Wrapper.m_Movement_Dash;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -291,6 +317,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Move.started += instance.OnMove;
             @Move.performed += instance.OnMove;
             @Move.canceled += instance.OnMove;
+            @Dash.started += instance.OnDash;
+            @Dash.performed += instance.OnDash;
+            @Dash.canceled += instance.OnDash;
         }
 
         /// <summary>
@@ -305,6 +334,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Move.started -= instance.OnMove;
             @Move.performed -= instance.OnMove;
             @Move.canceled -= instance.OnMove;
+            @Dash.started -= instance.OnDash;
+            @Dash.performed -= instance.OnDash;
+            @Dash.canceled -= instance.OnDash;
         }
 
         /// <summary>
@@ -352,5 +384,12 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnMove(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Dash" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDash(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
 
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashDuraion = 0.3f;
+    [SerializeField] private float alpha = 0.1f;
+
     private Vector2 moveDirection;
+    private float currentSpeed;
+    private bool canDash;
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        currentSpeed = moveSpeed;
+        canDash = true;
+        // Button 타입 액션에서는 입력 이벤트에 핸들러를 등록한다.
+        playerInputActions.Movement.Dash.performed += ctx => Dash();
     }
 
     private void OnEnable()
@@ -34,13 +51,61 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
+    /// <summary>
+    /// 유저 입력에 따라 이동 방향을 설정한다.
+    /// </summary>
     private void SetMoveDirection()
     {
+        // Value 타입 액션에서는 입력값을 출력값으로 변환한다.
         moveDirection = playerInputActions.Movement.Move.ReadValue<Vector2>().normalized;
     }
 
+    /// <summary>
+    /// 설정한 이동 방향으로 플레이어를 움직인다.
+    /// </summary>
     private void MovePlayer()
     {
-        rb.MovePosition(rb.position + moveDirection * (speed * Time.fixedDeltaTime));
+        // 콜리전(Collision)과 트리거(Trigger) 이벤트는 발생하지만, 다른 물리 법칙(예: 중력, 마찰, 관성 등)은 무시한다.
+        rb.MovePosition(rb.position + moveDirection * (currentSpeed * Time.fixedDeltaTime));
+    }
+
+    /// <summary>
+    /// 대시 이동할 때 플레이어의 투명도를 변경한다.
+    /// </summary>
+    /// <param name="alpha">투명도</param>
+    private void SetPlayerAlpha(float alpha)
+    {
+        Color initialColor = spriteRenderer.color;
+        Color newColor = new Color(initialColor.r, initialColor.g, initialColor.b, alpha);
+        spriteRenderer.color = newColor;
+    }
+
+    /// <summary>
+    /// 대시가 가능한 상태라면 대시 이동한다.
+    /// </summary>
+    private void Dash()
+    {
+        if (!canDash)
+            return;
+
+        StartCoroutine(DashCoroutine(dashDuraion));
+    }
+
+    /// <summary>
+    /// 대시 지속 시간 동안 플레이어의 투명도를 낮추고 스피드를 높인다.
+    /// 대시 지속 시간이 끝나면 플레이어의 투명도와 스피드를 되돌린다.
+    /// </summary>
+    /// <returns>코루틴</returns>
+    private IEnumerator DashCoroutine(float duration)
+    {
+        canDash = false;
+        currentSpeed = dashSpeed;
+        SetPlayerAlpha(alpha);
+
+        yield return new WaitForSeconds(duration);
+
+        currentSpeed = moveSpeed;
+        SetPlayerAlpha(1f);
+        canDash = true;
     }
 }
